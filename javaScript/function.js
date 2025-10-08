3d252400cfc7432889ae5e0f8b0f09ba6d0e86b5
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-08 14:44:37 +0600

    arrow function added

--- javaScript/function.js
+++ javaScript/function.js
@@ -21,3 +21,14 @@ console.log(multyply(2,3));
 setTimeout(function(){
     console.log("this runs after 2 sec");
 },2000);
+
+// arrow function
+
+const sum = (a,b) => a+b;
+
+console.log(sum(2,3));
+
+const s= (a,b)=>{
+    return a+b;
+}
+console.log(s(1,2));

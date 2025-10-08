134325bd7d5c8cbbc557a183bf55da258d9f4668
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-08 13:44:21 +0600

    anonymous function added

--- javaScript/function.js
+++ javaScript/function.js
@@ -9,3 +9,15 @@ function add (a,b)
 
 console.log(add(2,3));
 
+//anonymous function 
+
+const multyply =function(a,b)
+{
+    return a*b;
+}
+
+console.log(multyply(2,3));
+
+setTimeout(function(){
+    console.log("this runs after 2 sec");
+},2000);

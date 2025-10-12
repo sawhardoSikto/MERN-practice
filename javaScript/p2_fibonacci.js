9f201f32985b6cd6c64a69013de0f0c3cffe5a8b
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-13 00:32:22 +0600

    find fibonacci index

--- javaScript/p2_fibonacci.js
+++ javaScript/p2_fibonacci.js
@@ -1,11 +1,22 @@
 function getFibonacciSeries (n)
 {
     let series=[0,1];
-    for (let i = 2; i<n; i++)
+    for (let i = 2; i<= n; i++)
     {
         series[i] = series[i-1] + series[i-2];
         
     }
-    return series.slice(0,n);
+    return series
 }
-console.log(getFibonacciSeries(6));
+console.log(getFibonacciSeries(5));
+
+
+
+function findFibonacciIndex(n)
+{
+    const fibonacciSeries = getFibonacciSeries(n);
+    return fibonacciSeries[n];
+   
+}
+
+console.log(findFibonacciIndex(5));

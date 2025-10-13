7af5446eb28bc2d22ab91c3cf1b49b12cb25b0f6
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-13 16:51:06 +0600

    p_4_factorial added

---
+++ javaScript/p_4_factorial.js
@@ -0,0 +1,14 @@
+function factorial(n)
+{
+    let facto=1;
+    if (n===1 || n===0){
+        return 1;
+    }
+    for (let i =1 ; i<=n; i++ )
+    {
+        facto = facto*i;
+        
+    }
+    return facto;
+}
+console.log(factorial(5));

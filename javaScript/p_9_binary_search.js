7ac3b78e9e580a892df3e8365ca2c13c4be4787f
sawhardoSikto <143166338+sawhardoSikto@users.noreply.github.com>
2025-10-17 01:18:18 +0600

    p_9_binary_search added

--- javaScript/p_9_binary_search.js
+++ javaScript/p_9_binary_search.js
@@ -1,19 +1,16 @@
-function bubbleSort(arr)
-{
+function bubbleSort(arr) {
     let n = arr.length;
-    for (let i = 0; i<n-1; i++)
-    {
-        for (let j = 0; j<n-i-1; j++)
-        {
-            if (arr[j] > arr[j+1])
-            {
-                let temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+    let newArr = arr.slice(); // original array copy
+    for (let i = 0; i < n - 1; i++) {
+        for (let j = 0; j < n - i - 1; j++) {
+            if (newArr[j] > newArr[j + 1]) {
+                let temp = newArr[j];
+                newArr[j] = newArr[j + 1];
+                newArr[j + 1] = temp;
             }
         }
     }
-    return arr;
+    return newArr;
 }
 
 
